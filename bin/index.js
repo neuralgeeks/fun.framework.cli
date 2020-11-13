c6c998c7247945858079043445aaad5ca5627a39
Joao Pinto <joaobose@gmail.com>
2020-11-13 07:57:10 -0400

    Changed syntax of generate commands

--- bin/index.js
+++ bin/index.js
@@ -10,6 +10,7 @@ const program = new Command();
 
 let basePath = process.cwd();
 
+// -------------------------- init ---------------------------- //
 program
   .command('init <name>')
   .description('inits the a fun.framework project with the given name')
@@ -18,13 +19,27 @@ program
   });
 
 // TODO: this description is really bad. Learn to do it right.
+// --------------------- Generate service --------------------- //
 program
-  .command('generate <command> <resource> [name]')
+  .command('generate:service <name>')
   .description(
-    'generates a service or resource. command: resource/service. resource: resource name or service name. name: service name'
+    'generates a service or resource with the given name. name: the service name.'
   )
-  .action((command, resource, name) => {
-    generate(basePath, command, resource, name);
+  .action((name) => {
+    generate(basePath, 'service', { name: name });
+  });
+
+// --------------------- Generate resource --------------------- //
+program
+  .command('generate:resource <name> [service]')
+  .description(
+    "generates a resource inside the given service. name: the resource name. service: name of the service, default is 'API.service'"
+  )
+  .action((name, service) => {
+    generate(basePath, 'resource', {
+      name: name,
+      service: service
+    });
   });
 
 console.log(
