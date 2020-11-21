8b940331a4b7662c70583a0a5dc833caa619d4d0
Joao Pinto <joaobose@gmail.com>
2020-11-21 04:19:40 -0400

    Cleaning

--- bin/index.js
+++ bin/index.js
@@ -8,38 +8,30 @@ const generate = require('../lib/commands/generate.command');
 
 const program = new Command();
 
-let basePath = process.cwd();
+let path = process.cwd();
 
 // -------------------------- init ---------------------------- //
 program
-  .command('init <name>')
-  .description('inits the a fun.framework project with the given name')
-  .action((name) => {
-    init(basePath, name);
+  .command('init')
+  .description('inits the a fun.framework project')
+  .action(() => {
+    init(path);
   });
 
-// TODO: this description is really bad. Learn to do it right.
 // --------------------- Generate service --------------------- //
 program
-  .command('generate:service <name>')
-  .description(
-    'generates a service or resource with the given name. name: the service name.'
-  )
-  .action((name) => {
-    generate(basePath, 'service', { name: name });
+  .command('generate:service')
+  .description('generates a service inside a fun.framework project')
+  .action(() => {
+    generate(path, 'service');
   });
 
 // --------------------- Generate resource --------------------- //
 program
-  .command('generate:resource <name> [service]')
-  .description(
-    "generates a resource inside the given service. name: the resource name. service: name of the service, default is 'API.service'"
-  )
-  .action((name, service) => {
-    generate(basePath, 'resource', {
-      name: name,
-      service: service
-    });
+  .command('generate:resource')
+  .description('generates a Restful resource inside a fun.framework service.')
+  .action(() => {
+    generate(path, 'resource');
   });
 
 console.log(
