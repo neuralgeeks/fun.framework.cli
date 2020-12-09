aa4729a7b9decd01219252d5862ffd2d9902fc86
Joao Pinto <joaobose@gmail.com>
2020-12-09 01:09:12 -0400

    Removing routes comment from app.js template

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const { run } = require('./lib/schematics/API/engine');
 const { version } = require('./package.json');
 
-// run(
-//   `${__dirname}/temp/`,
-//   'init',
-//   { version: version },
-//   { dryRun: false, debug: true }
-// );
+run(
+  `${__dirname}/temp/`,
+  'init',
+  { version: version },
+  { dryRun: false, debug: true }
+);
 
 // run(
 //   `${__dirname}/temp/project`,
@@ -15,9 +15,9 @@ const { version } = require('./package.json');
 //   { dryRun: false, debug: false }
 // );
 
-run(
-  `${__dirname}/temp/project`,
-  'generate-resource',
-  { version: version },
-  { dryRun: false, debug: true }
-);
+// run(
+//   `${__dirname}/temp/project`,
+//   'generate-resource',
+//   { version: version },
+//   { dryRun: false, debug: true, force: false }
+// );
