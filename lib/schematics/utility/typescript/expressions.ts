6fcb624a3f06ba1a8b49f60eccc63b1d365d601a
Joao Pinto <joaobose@gmail.com>
2020-12-16 22:25:32 -0400

    Adding JSDoc

--- lib/schematics/utility/typescript/expressions.ts
+++ lib/schematics/utility/typescript/expressions.ts
@@ -1,5 +1,9 @@
 import * as ts from 'typescript';
 
+/**
+ * Creates a typescript source file given its UTF-8 content.
+ * Returns the ts.Node that represents the whole file.
+ */
 export function createSourceFile(
   content: string,
   name: string = 'file.ts'
@@ -7,6 +11,11 @@ export function createSourceFile(
   return ts.createSourceFile(name, content, ts.ScriptTarget.Latest, true);
 }
 
+/**
+ * Recursively matches a regex inside a ts.Node.
+ * Returns true if the ts.Node contains directly or indirectly (through children)
+ * a text that matches the regex.
+ */
 export function matchTextFromNode(node: ts.Node, regex: RegExp): boolean {
   if (node.getChildCount() === 0) return !!node.getText().match(regex);
 
