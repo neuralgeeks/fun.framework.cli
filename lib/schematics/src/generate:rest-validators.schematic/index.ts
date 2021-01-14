ce300e6037e83045c4606726b2d05d65b398e383
Joao Pinto <joaobose@gmail.com>
2021-01-14 01:10:48 -0400

    Adding license

--- lib/schematics/src/generate:rest-validators.schematic/index.ts
+++ lib/schematics/src/generate:rest-validators.schematic/index.ts
@@ -12,6 +12,23 @@ import { strings } from '@angular-devkit/core';
 import { plural } from 'pluralize';
 import { Schema } from './schema';
 
+/**
+ * @license
+ * Copyright 2020 neuralgeeks LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export function generateRestValidators(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     // Validate that root is a fun.framework project
